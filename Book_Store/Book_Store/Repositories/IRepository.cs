da5c8d952ff3a149734fbf06262ba7eeaf05b10a
Nantus Groenewald <nantusg@agilebridge.co.za>
2024-06-14 20:25:04 +0200

    Initial Project Commit
    Added application skeleton with implemented repository

---
+++ Book_Store/Book_Store/Repositories/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Book_Store.Repositories
+{
+    public interface IRepository<TEntity>: IDisposable
+    {
+        IEnumerable<TEntity> GetAll();
+        TEntity GetById(int id);
+        void Insert(TEntity model);
+        void Update(TEntity model);
+        void Delete(int id);
+        void Save(); 
+    }
+}
---
+++ Book_Store/Book_Store/Repositories/Repository.cs
@@ -0,0 +1,72 @@
+﻿using Book_Store.DAL;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Book_Store.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly BookStoreDBContext _context; 
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(BookStoreDBContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<TEntity>();
+        }
+
+        public IEnumerable<TEntity> GetAll()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public TEntity GetById(int id)
+        {
+            return _dbSet.Find(id); 
+        }
+
+        public void Insert(TEntity model)
+        {
+            _dbSet.Add(model);
+        }
+        public void Update(TEntity model)
+        {
+            _context.Entry(model).State = EntityState.Modified;
+        }
+        public void Delete(int id)
+        {
+            TEntity entity = _dbSet.Find(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+            }
+        }
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if(disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
